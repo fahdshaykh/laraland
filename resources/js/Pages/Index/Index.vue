52e98449d9c02ac9a8cea4b3bc87d7634534c0e1
fahd shaykh <fahdshaykh@outlook.com>
2023-11-03 22:10:23 +0500

    spa configure
    creating simple vue components and configure eslint for format

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        index view
+    </div>
+    <div>
+        welcome {{ message }}
+    </div>
+    <Link href="/hello">Show Page</Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+defineProps({
+    message: String
+})
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        show view
+    </div>
+    <Link href="/ ">Main Page</Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+</script>
